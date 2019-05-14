554755eabecdea72a5f78e081867da9b1391fe1f
양승무 <44221520+Ronalmoo@users.noreply.github.com>
2019-05-14 17:02:00 +0900

    # embedding_with_numpy

---
+++ embedding.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        return None
