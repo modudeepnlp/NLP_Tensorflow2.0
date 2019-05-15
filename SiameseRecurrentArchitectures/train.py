129a2730deeccddc21aefdf30699ff6d78e7c408
Ryan S. Shin <sungjin7127@gmail.com>
2019-05-15 09:42:56 +0900

    # update for SAN

--- SiameseRecurrentArchitectures/train.py
+++ SiameseRecurrentArchitectures/train.py
@@ -1,3 +1,8 @@
+"""
+Quora: 363861/363861 [==============================] - 79s 216us/sample - loss: 0.0927 - acc: 0.8756 - val_loss: 0.1586 - val_acc: 0.7846
+6200/6200 [==============================] - 10s 2ms/sample - loss: 0.2157 - acc: 0.6853 - val_loss: 0.1997 - val_acc: 0.7282
+"""
+
 import os
 import sys
 
@@ -13,37 +18,43 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 # from tensorflow.python.keras.models import Model, Sequential
 # from tensorflow.python.keras.layers import Input, Embedding, LSTM, GRU, Conv1D, Conv2D, GlobalMaxPool1D, Dense, Dropout, Bidirectional
 
-from lib.utils import make_w2v_embeddings
-from lib.utils import split_and_zero_padding
-from lib.utils import ManDist
+from model.utils import make_w2v_embeddings
+from model.utils import split_and_zero_padding
+
+# from lib.utils import ManDist
 
 from pathlib import Path
 
+proj_dir = Path.cwd()
+print(proj_dir)
+
 os.environ["CUDA_VISIBLE_DEVICES"]="7" #For TEST
 
+# question_train_quora = '../data/quora-question-pairs_eng/train.csv'
 question_train_kr = '../data/question-pair-kr/kor_pair_train.csv'
 question_test_kr = '../data/question-pair-kr/kor_pair_test.csv'
 
 train_df = pd.read_csv(question_train_kr)
+test_df = pd.read_csv(question_test_kr)
 
 for q in ['question1', 'question2']:
     train_df[q + '_n'] = train_df[q]
 # Make word2vec embeddings
 MAXLEN = 50
-EMB_DIM = 100
+EMB_DIM = 300
 use_w2v = False
 
 train_df, embeddings = make_w2v_embeddings(train_df, embedding_dim=EMB_DIM, empty_w2v=not use_w2v)
 
 # Model variables
 gpus = 1
-batch_size = 512 * gpus
-n_epoch = 50
+batch_size = 64 * gpus
+n_epoch = 100
 n_hidden = 50
 
 VOC_SIZE = len(embeddings)
@@ -84,11 +95,6 @@ class SelfAttention(tf.keras.Model):
 
         return att_layer
 
-# a = SelfAttention(10, 5)
-# b = np.random.rand(10,50)
-# a = a.call(b)
-# a.shape
-
 class SAN(tf.keras.Model):
 
     def __init__(self, max_len, emb_dim, n_hidden, a, d, vocab_size):
@@ -108,34 +114,36 @@ class SAN(tf.keras.Model):
         self.VOC_SIZE = vocab_size
 
         self._embedding = layers.Embedding(self.VOC_SIZE, self.EMB_DIM, input_length=self.MAX_LEN)
-        self._bilstm = layers.Bidirectional(layers.LSTM(n_hidden, return_sequences=True))
-        self._bilstm_last = layers.Bidirectional(layers.LSTM(n_hidden), input_shape=(50, 300))
+        self._bilstm = layers.Bidirectional(layers.LSTM(150, return_sequences=False), input_shape=(50, 300), name='shared_internal_layer')
+        # self._bilstm_last = layers.Bidirectional(layers.LSTM(n_hidden, return_sequences=False))
         self._attn = SelfAttention(a, d)
 
-        self._top_dense = layers.Dense(units=300, activation='relu')
+        self._top_dense = layers.Dense(units=100, activation='relu')
         self._last_dense = layers.Dense(units=1, activation='sigmoid')
+        # self._last_dense = layers.Dense(units=2, activation='softmax')
+        # self._last_dense = layers.Dense(units=2, activation='softmax')
 
     def call(self, x):
-
-        # print(x[0].shape)
-        # print(x[1].shape)
 
         q1_emb_layer = self._embedding(x[0])
         q2_emb_layer = self._embedding(x[1])
 
         q1_lstm = self._bilstm(q1_emb_layer)
-        q1_lstm = self._bilstm_last(q1_lstm)
+        # q1_lstm = self._bilstm_last(q1_lstm)
         q1_att = self._attn(q1_lstm)
 
         q2_lstm = self._bilstm(q2_emb_layer)
-        q2_lstm = self._bilstm(q2_lstm)
+        # q2_lstm = self._bilstm(q2_lstm)
+
+        print("q2_lstm pass", q2_lstm)
+
         q2_att = self._attn(q2_lstm)
 
-        print("pass", q2_att)
+        print("att pass", q2_att)
 
         Fr = q1_att * q2_att
 
-        print("pass", Fr)
+        print("fr pass", Fr)
 
         Fr = self._top_dense(Fr)
         Fr = self._top_dense(Fr)
@@ -144,9 +152,7 @@ class SAN(tf.keras.Model):
 
         return last
 
-# def __init__(self, max_len=30, emb_dim=300, n_hidden=100, a=30, d=300, vocab_size=None):
-
-sent_sim = SAN(max_len=MAXLEN, emb_dim=EMB_DIM, n_hidden=n_hidden, a=30, d=300, vocab_size=VOC_SIZE)
+sent_sim = SAN(max_len=MAXLEN, emb_dim=EMB_DIM, n_hidden=n_hidden, a=30, d=100, vocab_size=VOC_SIZE)
 
 if gpus >= 2:
     # `multi_gpu_model()` is a so quite buggy. it breaks the saved model.
@@ -156,8 +162,15 @@ sent_sim.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam()
 
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
                               min_delta=0,
-                              patience=3,
+                              patience=5,
                               verbose=0, mode='auto')
+
+# filepath = 'weights.h5'
+# checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath,
+#                                                 monitor='val_loss',
+#                                                 verbose=1,
+#                                                 save_best_only=True,
+#                                                 mode='min')
 
 # Start trainings
 training_start_time = time()
@@ -181,9 +194,8 @@ def plot_graphs(history, string):
 	plt.legend([string, 'val_'+string])
 	plt.savefig(string + '_ma_lstm.png')
 
-plot_graphs(history, 'accuracy')
-plot_graphs(history, 'loss')
+# plot_graphs(history, 'accuracy')
+# plot_graphs(history, 'loss')
 
-sent_sim.save_weights('./data/malstm', save_format='tf')
-
+sent_sim.save_weights('../data/san', save_format='tf')
 
