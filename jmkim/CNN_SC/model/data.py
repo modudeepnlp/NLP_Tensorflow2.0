08dd8b4f4615ecca2703d07f06d9a4ae6a6a3eed
jaeminkim87 <rlawoals2!>
2019-05-22 01:31:13 +0900

    Change Accuracy

--- jmkim/CNN_SC/model/data.py
+++ jmkim/CNN_SC/model/data.py
@@ -18,7 +18,8 @@ class Corpus():
         data, label = tf.io.decode_csv(item, record_defaults=[[''], [0]], field_delim='\t')
         data = [[self._vocab.token_to_idx[token] for token in self._toknizer.morphs(sen.numpy().decode('utf-8'))] for
                 sen in item]
-        data = pad_sequences(data, maxlen=FLAGS.length, value=self._vocab.token_to_idx['<pad>'], padding='post', truncating='post')
+        data = pad_sequences(data, maxlen=FLAGS.length, value=0,
+                             padding='post', truncating='post')
         #data = PadSequence(length=FLAGS.length, pad_val=self._vocab.token_to_idx['<pad>'])(data)
         data = tf.convert_to_tensor(data, dtype=tf.int32)
         label = tf.reshape(label, (item.get_shape()[0], ))
--- jmkim/CNN_SC/train.py
+++ jmkim/CNN_SC/train.py
@@ -14,97 +14,83 @@ from configs import FLAGS
 
 
 def main(argv):
-    train_data = Path.cwd() / 'data_in' / 'train.txt'
-    val_data = Path.cwd() / 'data_in' / 'val.txt'
+	train_data = Path.cwd() / 'data_in' / 'train.txt'
+	val_data = Path.cwd() / 'data_in' / 'val.txt'
 
-    with open(Path.cwd() / 'data_in' / 'vocab.pkl', mode='rb') as io:
-        vocab = pickle.load(io)
+	with open(Path.cwd() / 'data_in' / 'vocab.pkl', mode='rb') as io:
+		vocab = pickle.load(io)
 
-    train = tf.data.TextLineDataset(str(train_data)).shuffle(buffer_size=1000).batch(batch_size=FLAGS.batch_size)
-    eval = tf.data.TextLineDataset(str(val_data)).shuffle(buffer_size=1000).batch(batch_size=FLAGS.batch_size)
+	train = tf.data.TextLineDataset(str(train_data)).shuffle(buffer_size=1000).batch(batch_size=FLAGS.batch_size)
+	eval = tf.data.TextLineDataset(str(val_data)).shuffle(buffer_size=1000).batch(batch_size=FLAGS.batch_size)
 
-    tokenized = Mecab()
-    processing = Corpus(vocab=vocab, tokenizer=tokenized)
+	tokenized = Mecab()
+	processing = Corpus(vocab=vocab, tokenizer=tokenized)
 
-    # init params
-    classes = FLAGS.classes
-    max_length = FLAGS.length
-    epochs = FLAGS.epochs
-    batch_size = FLAGS.batch_size
-    learning_rate = FLAGS.learning_rate
+	# init params
+	classes = FLAGS.classes
+	max_length = FLAGS.length
+	epochs = FLAGS.epochs
+	batch_size = FLAGS.batch_size
+	learning_rate = FLAGS.learning_rate
 
-    # create model
-    sen_cnn = SenCNN(vocab=vocab, max_len=max_length, dim=batch_size, classes=classes)
+	# create model
+	sen_cnn = SenCNN(vocab=vocab, max_len=max_length, dim=batch_size, classes=classes)
 
-    # create optimizer & loss_fn
-    opt = tf.optimizers.Adam(learning_rate=learning_rate)
-    loss_fn = tf.losses.SparseCategoricalCrossentropy()
+	# create optimizer & loss_fn
+	opt = tf.optimizers.Adam(learning_rate=learning_rate)
+	loss_fn = tf.losses.SparseCategoricalCrossentropy()
 
-    # training
-    for epoch in tqdm(range(epochs)):
+	train_loss_metric = tf.keras.metrics.Mean(name='train_loss')
+	train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+	val_loss_metric = tf.keras.metrics.Mean(name='train_loss')
+	val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
 
-        tr_loss = 0
-        tr_acc = 0
-        tr_len = 0
-        tf.keras.backend.set_learning_phase(1)
+	# training
+	for epoch in tqdm(range(epochs)):
 
-        for step, val in enumerate(train):
-            data, label = processing.token2idex(val)
+		tr_loss = 0
+		tf.keras.backend.set_learning_phase(1)
+		train_loss_metric.reset_states()
+		train_acc_metric.reset_states()
 
-            '''
-            이 밑은 좀 공부 할것.
-            loss 관련
-            '''
-            with tf.GradientTape() as tape:
-                m = sen_cnn(data)
-                mb_loss = loss_fn(label, m)
-            grads = tape.gradient(target=mb_loss, sources=sen_cnn.trainable_variables)
-            opt.apply_gradients(grads_and_vars=zip(grads, sen_cnn.trainable_variables))
-            tr_loss += mb_loss.numpy()
+		for step, val in enumerate(train):
+			data, label = processing.token2idex(val)
 
-            prediction = tf.argmax(m, 1, name="predictions")
-            tr_acc += tf.reduce_sum(
-                tf.cast(tf.equal(tf.cast(prediction, tf.int64), tf.cast(label, tf.int64)), tf.int64))
-            tr_len += batch_size
+			'''
+			이 밑은 좀 공부 할것.
+			loss 관련
+			'''
+			with tf.GradientTape() as tape:
+				logits = sen_cnn(data)
+				pred_loss = loss_fn(label, logits)
+				total_loss = pred_loss
 
-        else:
-            '''
-            근데 이 else는 누구랑 세트인가??
-            if가 없는디.. python 문법에 이런게 있구나.
-            '''
-            tr_loss /= (step + 1)
-            tr_acc += tf.reduce_sum(
-                tf.cast(tf.equal(tf.cast(prediction, tf.int64), tf.cast(label, tf.int64)), tf.int64))
-            tr_len += batch_size
+			grads = tape.gradient(target=total_loss, sources=sen_cnn.trainable_variables)
+			opt.apply_gradients(grads_and_vars=zip(grads, sen_cnn.trainable_variables))
 
-        tf.keras.backend.set_learning_phase(0)
+			train_loss_metric.update_state(total_loss)
+			train_acc_metric.update_state(label, logits)
 
-        val_loss = 0
-        val_acc = 0
-        val_len = 0
-        for step, val in enumerate(eval):
-            data, label = processing.token2idex((val))
-            m = sen_cnn(data)
-            mb_loss = loss_fn(label, m)
-            val_loss += mb_loss.numpy()
-            prediction = tf.argmax(m, 1, name="predictions")
-            val_acc += tf.reduce_sum(tf.cast(tf.equal(tf.cast(prediction,tf.int64), tf.cast(label, tf.int64)), tf.int64))
-            val_len += batch_size
-        else:
-            val_loss /= (step + 1)
-            val_acc += tf.reduce_sum(tf.cast(tf.equal(tf.cast(prediction,tf.int64), tf.cast(label, tf.int64)), tf.int64))
-            val_len += batch_size
+		tr_loss = train_loss_metric.result()
+				  #/ (step + 1)
 
-        train_accu = tr_acc / tr_len * 100
-        val_accu = val_acc / val_len * 100
+		tf.keras.backend.set_learning_phase(0)
 
-        print("tr_acc {}".format(tr_acc))
-        print("tr_len {}".format(tr_len))
-        print("val_acc {}".format(val_acc))
-        print("val_len {}".format(val_len))
+		val_loss = 0
+		for step, val in enumerate(eval):
+			data, label = processing.token2idex((val))
+			logits = sen_cnn(data)
+			mb_loss = loss_fn(label, logits)
+			val_loss_metric.update_state(mb_loss)
+			val_acc_metric.update_state(label, logits)
 
-        tqdm.write('epoch : {}, tr_acc : {:.3f}%, tr_loss : {:.3f}, val_acc : {:.3f}%, val_loss : {:.3f}'.format(epoch + 1, train_accu, tr_loss, val_accu, val_loss))
+		val_loss = val_loss_metric.result()
+				   #/ (step + 1)
+
+		print("tr_acc {}%".format(train_acc_metric.result() * 100))
+
+		tqdm.write('epoch : {}, tr_acc : {:.3f}%, tr_loss : {:.3f}, val_acc : {:.3f}%, val_loss : {:.3f}'.format(epoch + 1, train_acc_metric.result() * 100, tr_loss, val_acc_metric.result() * 100, val_loss))
 
 
 if __name__ == '__main__':
-    app.run(main)
+	app.run(main)
